9936618c32f9de19498681a5a4907be51cef3527
Gonagi <kkh10002@naver.com>
2023-07-19 16:04:36 +0900

    좌표 변환 및 변수 수정, 출발지/도착지 묶음, 구간 정보 개선     - 위도, 경도 / X, Y 간의 변환을 위한 'dfs_xy_conv'함수 사용하기 위해 require 추가     - 경도(lon), 위도(lat)혼동으로 인한 변수 이름 수정     - 위도, 경도를 좌표(X, Y)로 통일     - 출발지와 도착지 좌표를 하나로 묶음     - 구간의 출발지와 도착지 정보를 각각 이름, 좌표로 묶음     - 환승(TRANSFER)을 도보(WALK)로 추가 처리

--- public_transport.js
+++ public_transport.js
@@ -1,4 +1,4 @@
-const { response } = require("express");
+const convert = require('./convert_XY.js'); // 같은 경로에 있는 convert_XY.js파일의 dfs_xy_conv함수를 쓴다.
 
 const options = {
     method: 'POST',
@@ -8,8 +8,8 @@ const options = {
         appKey: 'e8wHh2tya84M88aReEpXCa5XTQf3xgo01aZG39k5'
     },
     body: JSON.stringify({
-        startX: "126.9955",
-        startY: "37.5038",
+        startX: "127.2157", // 경도(lon) x
+        startY: "37.5814",  // 위도(lat) y
         endX: "126.96",
         endY: "37.4945",
         lang: 0,
@@ -19,20 +19,21 @@ const options = {
     })
 };
 
-// 위도(lat) x 37 
-// 경도(lon) y 126 
-
 fetch('https://apis.openapi.sk.com/transit/routes', options)
     .then(response => response.json())
     .then(road_data => {
         if (road_data.metaData && road_data.metaData.plan && road_data.metaData.plan.itineraries && road_data.metaData.plan.itineraries.length > 0) {
 
-            const start_lat = road_data.metaData.requestParameters.start_lat;
-            const start_lon = road_data.metaData.requestParameters.start_lon;
-            const end_lat = road_data.metaData.requestParameters.end_lat;
-            const end_lon = road_data.metaData.requestParameters.end_lon;
+            const start_lat = road_data.metaData.requestParameters.startY;
+            const start_lon = road_data.metaData.requestParameters.startX;
+            const starting_point = convert.dfs_xy_conv("toXY", `${start_lat}`, `${start_lon}`);   // 출발지 위경도 --> XY
+
+            const end_lat = road_data.metaData.requestParameters.endY;
+            const end_lon = road_data.metaData.requestParameters.endX;
+            const end_point = convert.dfs_xy_conv("toXY", `${end_lat}`, `${end_lon}`);            // 목적지 위경도 --> XY
+
             const itinerary = road_data.metaData.plan.itineraries;
-            const itinerary_count = itinerary.length;                                       // 루트 개수
+            const itinerary_count = itinerary.length;                                             // 루트 개수
 
             // 루트 개수만큼 반복(5번)
             for (cur_itinerary = 0; cur_itinerary < itinerary_count; cur_itinerary++) {
@@ -40,34 +41,47 @@ fetch('https://apis.openapi.sk.com/transit/routes', options)
                 const transferCount = itinerary[cur_itinerary].transferCount;
                 const method = itinerary[cur_itinerary].legs;
                 const section_count = method.length;
-                const sections = [];                                                        // 구간 객체배열
+                const sections = [];    // 구간 객체배열
 
                 // 구간 개수만큼 반복
                 for (cur_section = 0; cur_section < section_count; cur_section++) {
                     const section_start_name = method[cur_section].start.name;
                     const section_start_lat = method[cur_section].start.lat;
                     const section_start_lon = method[cur_section].start.lon;
+                    const section_start = convert.dfs_xy_conv("toXY",
+                        `${section_start_lat}`, `${section_start_lon}`);    // 구간의 출발지 위경도 --> XY
+
+
                     const section_end_name = method[cur_section].end.name;
                     const section_end_lat = method[cur_section].end.lat;
                     const section_end_lon = method[cur_section].end.lon;
+                    const section_end = convert.dfs_xy_conv("toXY",
+                        `${section_end_lat}`, `${section_end_lon}`);       // 구간의 목적지 위경도 --> XY
+
                     const sectionTime = method[cur_section].sectionTime;
                     const distance = method[cur_section].distance;
 
                     const section = {
-                        "section_start_name": section_start_name,   // 구간의 출발지 이름
-                        "section_start_lat": section_start_lat,     // 구간의 출발지 위도
-                        "section_start_lon": section_start_lon,     // 구간의 출발지 경도
-                        "section_end_name": section_end_name,       // 구간의 목적지 이름
-                        "section_end_lat": section_end_lat,         // 구간의 목적지 위도
-                        "section_end_lon": section_end_lon,         // 구간의 목적지 경도
-                        "sectionTime": sectionTime,                 // 구간의 소요시간(s)
-                        "distance": distance                        // 구간의 거리(m)
+                        section_start: {                           // 구간의 출발지 이름, X, Y
+                            name: section_start_name,
+                            X: section_start.x,
+                            Y: section_start.y
+                        },
+                        section_end: {                              // 구간의 목적지 이름, X, Y
+                            name: section_end_name,
+                            X: section_end.x,
+                            Y: section_end.y
+                        },
+                        sectionTime: sectionTime,                   // 구간의 소요시간(s)
+                        distance: distance                          // 구간의 거리(m)
                     };
 
-                    if (method[cur_section].mode == "WALK")                                                 // 구간의 이동수단이 WALK일때
+                    if (method[cur_section].mode == "WALK" ||
+                        method[cur_section].mode == "TRANSFER")         // 구간의 이동수단이 도보 or 환승일때
                         section.mode = "WALK";
 
-                    else if (method[cur_section].mode == "BUS" || method[cur_section].mode == "SUBWAY") {   // 이동수단이 버스 or 지하철일때
+                    else if (method[cur_section].mode == "BUS"
+                        || method[cur_section].mode == "SUBWAY") {   // 이동수단이 버스 or 지하철일때
                         section.mode = `${method[cur_section].mode}`;                                       // 이동수단
                         section.route_name = `${method[cur_section].route}`;                                // 노선 이름
                         section.route_color = `${method[cur_section].routeColor}`;                          // 노선 색
@@ -78,10 +92,15 @@ fetch('https://apis.openapi.sk.com/transit/routes', options)
                 }
 
                 const Route = {
-                    "start_lat": start_lat,         // 출발지 위도
-                    "start_lon": start_lon,         // 출발지 경도
-                    "end_lat": end_lat,             // 목적지 위도
-                    "end_lon": end_lon,             // 목적지 경도
+                    Start: {    // 출발지 X, Y
+                        X: starting_point.x,
+                        Y: starting_point.y
+                    },
+
+                    End: {      // 목적지 X, Y
+                        X: end_point.x,
+                        Y: end_point.y
+                    },
                     "totalTime": totalTime,         // 총 소요시간
                     "transferCount": transferCount, // 환승 횟수
                     "section_count": section_count, // 구간 개수
