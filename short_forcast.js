e28f3f6dc05aa2b8f1918ff7ef9441e3e37ffbcc
Gonagi <kkh10002@naver.com>
2023-07-22 23:12:26 +0900

    단기예보API를 이용한 getShortForecastData함수     - 현재시간을 포함한 앞으로 6시간동안의 강수확률을 short_forcast_data객체에 넣어 리턴한다.     - 강수확률을 받아오지 못하는 특정시간은 '이전 단기예보'와 '현재 단기예보'를 병합하여 처리한다.     - 날짜(YYYYMMDD), 시간(HHMM), 좌표(x, y)를 파라미터로 입력받는다.     - 시간의 '분'을 버려 현재시간을 계산하고 API제공시간에 따라 base_time을 정리한다.

--- short_forcast.js
+++ short_forcast.js
@@ -1,48 +1,152 @@
 // 단기예보
 // 강수확률(POP)
-
 const { response } = require('express');
+const fetch = require('node-fetch');
 
-import('node-fetch').then((fetch) => {
-    const url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst'; /*URL*/
-    const serviceKey = 'YVQkpHY14ykgf2l2ayZx+wOfqTGaLVjka0T1pl7g4PsQuB6cGA/wrf5j8AT9dKraniz5z8rTowBF2RM0W+8jkg==';
+const url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst'; /*URL*/
+const serviceKey = 'YVQkpHY14ykgf2l2ayZx+wOfqTGaLVjka0T1pl7g4PsQuB6cGA/wrf5j8AT9dKraniz5z8rTowBF2RM0W+8jkg==';
 
-    // 02:10시를 기준, 3시간 간격으로 발표 (02:10, 05:10, 08:10, 11:10, ...)
-    // 입력시간을 기준으로 가장가까운 시각 basetime으로 넣을 예정
-    const queryParams = new URLSearchParams({
-        serviceKey,
-        pageNo: '1',
-        numOfRows: '1000',
-        dataType: 'JSON',
-        base_date: '20230721',
-        base_time: '2000',
-        nx: '59',
-        ny: '125'
-    });
-
-    fetch.default(url + '?' + queryParams)
+function fetchForecastData(queryParams) {
+    return fetch(url + '?' + queryParams)
         .then(response => response.json())
-        .then(data => {
-            const items = data.response.body.items.item;
-            const short_forcast_data = {};
-            let check = 0;
+        .then(data => data.response.body.items.item)
+        .catch(error => console.error(error));
+}
 
-            for (idx = 0; ; idx++) {
-                const fcstTime = items[idx].fcstTime;
+// 단기예보API로 앞으로 6시간의 강수확률을 받아오는 함수
+async function getShortForecastData(input_date, input_time, input_x, input_y) {
+    // 입력한 시간과 가장 가까운 base_time을 구하고 현재시각의 '분'을 버려 정리한다.
+    let cal_time = parseInt(input_time, 10);
+    let cal_base_time = Math.floor((cal_time + 90) / 300) * 300 - 100;
 
-                if (check == 6)
-                    break;
+    if (cal_base_time === -100)
+        cal_base_time = 2300;
 
-                if (items[idx].category === "POP") {         // 강수확률(POP)   
-                    short_forcast_data[fcstTime] = {};       // 예보시간에 따른 날씨데이터
-                    short_forcast_data[fcstTime][items[idx].category] = items[idx].fcstValue;
-                    check++;
-                }
-            }
-            console.log(short_forcast_data);
-        })
-        .catch(error => {
-            console.error(error);
+    cal_time = Math.floor(cal_time / 100) * 100;
+    next_time = Math.floor(cal_time / 100) * 100 + 100;
+
+    if (next_time === 2400)
+        next_time = 0;
+
+    const baseTimes = [
+        { time: 200, base: 2300 },
+        { time: 500, base: 200 },
+        { time: 800, base: 500 },
+        { time: 1100, base: 800 },
+        { time: 1400, base: 1100 },
+        { time: 1700, base: 1400 },
+        { time: 2000, base: 1700 },
+        { time: 2300, base: 2000 }
+    ];
+
+    let short_forcast_data = {};
+
+    const cal_time_str = cal_time.toString().padStart(4, '0');
+    const cal_base_time_str = cal_base_time.toString().padStart(4, '0');
+
+    const next_time_str = next_time.toString().padStart(4, '0');
+
+    // 현재시간이 특정시간(0200, 0500, 0800, 1100, 1400, 1700, 2000, 2300)일때
+    // 1700일때
+    if (baseTimes.some(({ time, base }) => time === cal_time)) {
+        const prev_base_time_str = baseTimes.find(({ time }) => time === cal_time).base.toString().padStart(4, '0');
+
+        // 3시간전의 단기예보 API를 돌린다
+        // 14시의 단기예보 API를 돌린다
+        const prevQueryParams = new URLSearchParams({
+            serviceKey,
+            pageNo: '1',
+            numOfRows: '1000',
+            dataType: 'JSON',
+            base_date: input_date,
+            base_time: prev_base_time_str,
+            nx: input_x,
+            ny: input_y
         });
-});
+        const prevForecastData = await fetchForecastData(prevQueryParams);
 
+        // 현재 시간의 강수확률을 구해 forecastData에 넣는다
+        // 17시의 강수확률 구해 forecastData에 넣는다
+        const fcstTimeData = prevForecastData.find(item => item.fcstTime === cal_time_str && item.category === 'POP');
+        if (fcstTimeData)
+            short_forcast_data[cal_time_str] = { POP: fcstTimeData.fcstValue };
+
+        // 17시의 단기예보 API를 돌린다
+        const queryParams = new URLSearchParams({
+            serviceKey,
+            pageNo: '1',
+            numOfRows: '1000',
+            dataType: 'JSON',
+            base_date: '20230722',
+            base_time: cal_base_time_str,
+            nx: '59',
+            ny: '125'
+        });
+        const forecastData = await fetchForecastData(queryParams);
+
+        // 나머지 5시간의 강수확률을 구한다
+        // 1800 1900 2000 2100 2200의 강수확률을 구해 forecastData에 넣는다
+        let check = 0;
+        for (const item of forecastData) {
+            const fcstTime = item.fcstTime;
+
+            if (check === 5)
+                break;
+
+            if (item.category === "POP" && (fcstTime === next_time_str || check > 0)) {
+                short_forcast_data[fcstTime] = { POP: item.fcstValue };
+                check++;
+            }
+        }
+        // console.log(short_forcast_data);
+
+    }
+
+    // 특정시간이 아닐때
+    // 가까운 base_time으로 6시간의 강수확률을 forecastData에 넣는다.
+    else {
+        const queryParams = new URLSearchParams({
+            serviceKey,
+            pageNo: '1',
+            numOfRows: '1000',
+            dataType: 'JSON',
+            base_date: '20230722',
+            base_time: cal_base_time_str,
+            nx: '59',
+            ny: '125'
+        });
+
+        const forecastData = await fetchForecastData(queryParams);
+
+        let check = 0;
+
+        for (const item of forecastData) {
+            const fcstTime = item.fcstTime;
+
+            if (check === 6)
+                break;
+
+            if (item.category === "POP" && (fcstTime === cal_time_str || check > 0)) {
+                short_forcast_data[fcstTime] = { POP: item.fcstValue };
+                check++;
+            }
+        }
+
+        //console.log(short_forcast_data);
+    }
+    return short_forcast_data;
+}
+
+// 사용 예시
+const input_date = '20230722'
+const input_time = '2109';
+const input_x = '59';
+const input_y = '125';
+
+getShortForecastData(input_date, input_time, input_x, input_y)
+    .then(POP_data => {
+        console.log(POP_data);
+    })
+    .catch(error => {
+        console.error(error);
+    });
