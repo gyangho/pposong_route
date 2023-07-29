ab18a90cc33e0d7500c71f739ce0fa18eb969563
Gonagi <kkh10002@naver.com>
2023-07-29 11:29:11 +0900

    get_4time, pposong_time함수 추가     - get_4time : 30분간격으로 2시간동안의 시간을 리턴함     - pposong_time : get_4time으로 얻은 시각에 출발해 맞는 비의 양을 계산함

---
+++ main.js
@@ -0,0 +1,21 @@
+const fetchData_transport = require('./public_transport _2');
+const pposong_time = require('./pposong_time.js');
+
+let input_date = 20230728;
+let input_time = '2310';
+
+const times = pposong_time.get_4time(input_date, input_time, 100);
+
+console.log(times);
+
+fetchData_transport.getPublicTransport(126.9961, 37.5035, 126.96, 37.4946, 202307281200)
+    .then(routes => {
+        console.log(routes[0]);
+        const result = pposong_time.cal_pposong_time(input_date, input_time, routes[0]);    // 원하는 루트, 원하는 시간
+
+        console.log(result.pposong_results1);
+        console.log(result.pposong_results2);
+    })
+    .catch(error => {
+        console.error(error);
+    });
---
+++ pposong_time.js
@@ -0,0 +1,95 @@
+const calculate = require('./cal_time_date.js');
+const pposong_time = require('./pposong_time.js');
+
+
+function cal_pposong_time(input_date, input_time, route) {
+    const times = get_4time(input_date, input_time, 100);   // 30분간격 4time
+    const pposong_results1 = [];    // 구간별 결과들
+    const pposong_results2 = [];    // 총 결과들
+
+    times.forEach(time => {
+        const pposong_result = [];  // 구간의 결과
+        let start_time = time.Time;
+        let end_time = start_time;
+        let start_date = time.Date;
+        let end_date = start_date;
+        let RN1_sum = 0;
+        let RN1;
+
+        route.sections.forEach(section => {
+            end_time = parseInt(start_time, 10);
+            let add_time = Math.round(section.sectionTime / 60);
+            end_time += add_time;
+            if (end_time % 100 >= 60) {
+                end_time += 40;
+                if (end_time >= 2400) {
+                    end_date = calculate.get_next_basedate(start_date);
+                    end_time -= 2400;
+                }
+            }
+
+            let hours = String(Math.floor(end_time / 100)).padStart(2, '0');
+            let minutes = String(end_time % 100).padStart(2, '0');
+            end_time = hours + minutes;
+
+            if (section.mode === 'WALK') {
+                let X = section.X;
+                let Y = section.Y;
+                // db에서 RN1받아옴
+                // let RN1 = db에서 base_time의 X, Y 강수량 받아옴;
+                RN1 = 3;   // 예시
+
+                RN1 = parseFloat((RN1 * section.sectionTime * 100 / 60 / 100 / 60).toFixed(2)); // 소수점 둘째짜리까지
+                RN1_sum += RN1;
+            }
+
+            else   // 이동수단이 BUS, SUBWAY인경우
+                RN1 = 0;
+
+            pposong_result.push({
+                DATE: start_date,
+                START_TIME: start_time,
+                END_TIME: end_time,
+                RN1: RN1,
+            });
+            start_time = end_time;
+            start_date = end_date;
+
+        });
+        pposong_results1.push(pposong_result);
+        pposong_results2.push({ START_TIME: time.Time, END_TIME: end_time, RN1_SUM: RN1_sum });
+    });
+    return { pposong_results1, pposong_results2 };
+
+}
+
+function get_4time(input_date, input_time, time_to_divide) {
+    let cur_time = parseInt(input_time, 10);
+    let cur_base_date = input_date;
+
+    let times = [];
+
+    for (idx = 0; idx < 4; idx++) {
+        let cur_base_time = Math.floor(cur_time / time_to_divide) * time_to_divide;
+
+        let cur_time_str = cur_time.toString().padStart(4, '0');
+        let cur_base_time_str = cur_base_time.toString().padStart(4, '0');
+
+        times.push({ Date: cur_base_date, Time: cur_time_str, BaseTime: cur_base_time_str });
+
+        cur_time += 30;
+        if (cur_time % 100 >= 60)
+            cur_time += 40
+        if (cur_time >= 2400) {
+            cur_time -= 2400
+            cur_base_date = calculate.get_next_basedate(cur_base_date);
+        }
+    }
+
+    return times;
+}
+
+module.exports = {
+    get_4time: get_4time,
+    cal_pposong_time: cal_pposong_time
+};
