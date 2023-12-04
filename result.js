4b3120cac5dce287219bfedca387acc073729445
Gonagi <kkh10002@naver.com>
2023-12-05 01:00:27 +0900

    12.05

--- result.js
+++ result.js
@@ -31,7 +31,6 @@ async function createDynamicHTML(data) {
     var PATH = "";
     if (Routes.length > 0) {
       Routes.map((Path) => {
-        console.log("Path:", Path);
         var SUBPATH = "";
         const hours = Math.floor(Path.TotalTime / 60); //시간 계산
         const minutes = Path.TotalTime % 60; //분 계산
@@ -59,9 +58,10 @@ async function createDynamicHTML(data) {
                             ${SubPath.StationCount}개 역<br>   
                         </div>`;
               vertical_bar = `
-                        <div class="route-list__bar" style="border-left: thick solid green">
+                        <div class="route-list__bar" style="border-left: thick solid ${SubPath.SubwayColor};">
                             ${SubPath.SectionTime}분
                         </div>`;
+
               break;
             case "BUS": //버스이면
               const VehicleIcons = SubPath.LaneInfo.map(
@@ -81,8 +81,8 @@ async function createDynamicHTML(data) {
                             ${SubPath.StationCount}정거장<br> 
                         </div>`;
               vertical_bar = `
-                    <div class="route-list__bar" style="border-left: thick solid blue">
-                            ${SubPath.SectionTime}분
+                        <div class="route-list__bar" style="border-left: thick solid ${SubPath.LaneInfo[0].BusColor};">
+                                ${SubPath.SectionTime}분
                         </div>`;
               break;
             case "WALK":
@@ -92,14 +92,18 @@ async function createDynamicHTML(data) {
                         <div>${SubPath.Distance}m<br>
                         </div>`;
                 vertical_bar = `
-                        <div class="route-list__bar" style="border-left: thick dotted black">
+                        <div class="route-list__bar" style="border-left: thick dotted black;">
                             ${SubPath.SectionTime}분
-                            </div>`;
+                        </div>`;
               }
               break;
           }
+          //div class="reset_bar" height 수정 line107
+          //수정 2023.12.1 채수아
           SUBPATH += `
-                <div class="reset">
+                <div class="reset_bar" style="height:${
+                  (SubPath.SectionTime / Path.TotalTime) * 100
+                }%;"> 
                         ${vertical_bar}
                     <div class="route-list__vehicle">
                         <div class="route-list__vehicle-info">
@@ -110,24 +114,27 @@ async function createDynamicHTML(data) {
                </div>
             `;
         });
-
+        //div class="total"수정 line 134
+        //수정 2023.12.1 채수아
         PATH += `
-            <div class = "route-list">
-            <div class="route-list__column">
-            <div class="route-list__time">
-                    <h4 class="route-list__total-time">총 소요시간 : ${TotalTimeString}</h4>
-                    <h6 class="route-list__walk-time">총 도보 시간 : ${Path.TotalWalkTime}분</h6>
-                    <h6 class="route-list__walk-time">총 도보 거리 : ${Path.TotalWalk}m</h6>
-                    <h6 class="route-list__walk-time">가격 : ${Path.Payment}원</h6>
-                </div>
-                <div class="route-list__bookmark">
-                    <i class="fa-regular fa-star fa-xl"></i>
-                </div>
+          <div class="route-list">
+          <div class="route-list__column">
+              <div class="route-list__time">
+                  <h4 class="route-list__total-time">총 소요시간 : ${TotalTimeString}</h4>
+                  <h6 class="route-list__walk-time">총 도보 시간 : ${Path.TotalWalkTime}분</h6>
+                  <h6 class="route-list__walk-time">총 도보 거리 : ${Path.TotalWalk}m</h6>
+                  <h6 class="route-list__walk-time">가격 : ${Path.Payment}원</h6>
+              </div>
+              <div class="route-list__bookmark">
+                  <i class="fa-regular fa-star fa-xl"></i>
+              </div>
             </div>
             <div class="route-list__vehicle">${resource.start}</div>
-            ${SUBPATH}
-            <div class="route-list__vehicle">${resource.end}</div>    
-            </div>`;
+            <div class="total">
+                ${SUBPATH}
+            </div>
+            <div class="route-list__vehicle">${resource.end}</div>
+        </div>`;
       });
     }
 
@@ -135,9 +142,9 @@ async function createDynamicHTML(data) {
 
     const routeJSON = JSON.stringify(Routes);
     const finalTemplate = `${modifiedTemplate}
-    <script>
-      var Routes = ${routeJSON}
-    </script>`;
+        <script>
+            var Routes = ${routeJSON}
+        </script>`;
     return finalTemplate;
 
     //return modifiedTemplate;
